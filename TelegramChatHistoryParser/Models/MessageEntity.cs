96d8238706924b4833a1154d2ab1fea912e3549b
Shepelev Alexander <alexander@shepelev.pro>
2023-04-18 00:45:57 +0300

    Add models

---
+++ TelegramChatHistoryParser/Models/MessageEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TelegramChatHistoryParser.Models
+{
+    public class MessageEntity
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Edited { get; set; }
+        public string From { get; set; }
+        public string FromId { get; set; }
+        public string? Photo { get; set; }
+    }
+}
