c1df2a50a7805619dd6cf381dcaae3097659928b
Shepelev Alexander <alexander@shepelev.pro>
2023-04-18 00:58:19 +0300

    Add Parser class & Exceptions

---
+++ TelegramChatHistoryParser/TelegramHistoryParser.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+using TelegramChatHistoryParser.Exceptions;
+using TelegramChatHistoryParser.Models;
+
+namespace TelegramChatHistoryParser
+{
+    public class TelegramHistoryParser
+    {
+        public TelegramHistoryParser()
+        {
+        }
+        
+        private FileEntity ParseJson(Stream stream)
+        {
+            var result = JsonSerializer.Deserialize<FileEntity>(stream);
+            if (result == null)
+                throw new InvalidJsonFileException();
+            return result;
+        }
+
+        public FileEntity Parse(Stream stream)
+        {
+            return ParseJson(stream);
+        }
+
+        public FileEntity Parse(string json)
+        {
+            return ParseJson(new MemoryStream(Encoding.ASCII.GetBytes(json)));
+        }
+    }
+}
